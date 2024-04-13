8bcab8a77495b5574d0a0753e8049c39ca0fe37e
Abhi-y2003 <abhisheks2651@gmail.com>
2024-04-13 17:10:36 +0530

    profile deletion

--- models/Course.js
+++ models/Course.js
@@ -35,6 +35,9 @@ const courseSchema = new mongoose.Schema({
     thumbnail:{
         type:String
     },
+    tag:{
+        type:String
+    },
     category:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"category"
